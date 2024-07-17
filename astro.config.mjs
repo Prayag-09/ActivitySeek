2bd394c3d930e0a2e98e8114c3ac84640e5dfeb7
Prayag <prayagtushar2016@gmail.com>
2024-07-17 19:24:00 +0530

    Final Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [react()],
-  adapter: netlify(),
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
